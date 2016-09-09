7a189a978c6cb534ca51d2a3a815770d9424a2b7
Thiago Della Libera Moreira <thiago.dlm@gmail.com>
2016-09-09 12:26:50 -0300

    Caminho absoluto para pasta desorganizada sendo lido pela linha de comando, o usuário não precisa mais editar o arquivo .js

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
+const argv = require('yargs').option('path',{
+	alias: 'p',
+	demand: true,
+	describe: 'O diretório (entre aspas) onde estão suas fotos \ndesorganizadas.',
+	type: 'string'
+}).help('help').argv;;
 
-var diretorio = './pasta-de-destino-dos-arquivos';
-var months = {
-	0: 'Janeiro',
-	1: 'Fevereiro',
-	2: 'Marco',
-	3: 'Abril',
-	4: 'Maio',
-	5: 'Junho',
-	6: 'Julho',
-	7: 'Agosto',
-	8: 'Setembro',
-	9: 'Outubro',
-	10: 'Novembro',
-	11: 'Dezembro'
-  };
-  
-files = fs.readdirSync(diretorio);
-for(i = 0; i < files.length; i++){
-	var name  = files[i];
-	var file  = fs.statSync(diretorio + '/' + files[i]);
-	var day   = new Date(util.inspect(file.mtime)).getDate();
-	var month = new Date(util.inspect(file.mtime)).getMonth();
-	var year  = new Date(util.inspect(file.mtime)).getFullYear();
 
-	dirYear   = diretorio + '/' + year;
-	dirMonth  = diretorio + '/' + year + '/' + months[month];
 
-	if (!fs.existsSync(dirYear)){
-	fs.mkdirSync(dirYear);
+if(argv.path != undefined && argv.path.length > 0){
+
+	var diretorio = argv.path;
+	var months = {
+		0: 'Janeiro',
+		1: 'Fevereiro',
+		2: 'Marco',
+		3: 'Abril',
+		4: 'Maio',
+		5: 'Junho',
+		6: 'Julho',
+		7: 'Agosto',
+		8: 'Setembro',
+		9: 'Outubro',
+		10: 'Novembro',
+		11: 'Dezembro'
+	  };
+	  
+	files = fs.readdirSync(diretorio);
+	for(i = 0; i < files.length; i++){
+		var name  = files[i];
+		var file  = fs.statSync(diretorio + '/' + files[i]);
+		var day   = new Date(util.inspect(file.mtime)).getDate();
+		var month = new Date(util.inspect(file.mtime)).getMonth();
+		var year  = new Date(util.inspect(file.mtime)).getFullYear();
+
+		dirYear   = diretorio + '/' + year;
+		dirMonth  = diretorio + '/' + year + '/' + months[month];
+
+		if (!fs.existsSync(dirYear)){
+		fs.mkdirSync(dirYear);
+		}
+
+		if(!fs.existsSync(dirMonth)){
+		fs.mkdirSync(dirMonth)
+		}
+
+		var oldPath = diretorio + '/' + name;
+		var newPath = diretorio + '/' + year + '/' + months[month] + '/' + name;
+
+		fs.renameSync(oldPath, newPath); 
 	}
-
-	if(!fs.existsSync(dirMonth)){
-	fs.mkdirSync(dirMonth)
-	}
-
-	var oldPath = diretorio + '/' + name;
-	var newPath = diretorio + '/' + year + '/' + months[month] + '/' + name;
-
-	fs.renameSync(oldPath, newPath); 
+}else{
+	console.log("\n\nError: Por favor insira uma path válida.\n\n")
 }
